98008dc834012427d4c00d4d117a9152a6c83f54
kitze012 <sskitzerow@gmail.com>
2024-01-06 14:32:43 -0600

    Adding first practice problem

---
+++ string_to_matrix.py
@@ -0,0 +1,36 @@
+'''
+Convert a string to a matrix with the following pattern:
+    5.5,100,5.0,102,6.0,103:L10;5.5,101,5.0,105,6.0,99:L20;.....
+
+The resulting matix should look like the following:
+
+            lock_1      lock_2      ...     lock_m
+    rate_1  price_1_1   price_1_2           price_1_m
+
+    rate_2  price_2_1
+
+    ...
+
+    rate_n  price_n_1   ...                 price_n_m
+
+
+The example string will give:
+
+            10      20
+    5.5     100     101
+
+    5.0     102     105
+
+    6.0     103     99
+'''
+
+def convert_string(string:str):
+
+
+
+    return
+
+
+
+if __name__ == "__main__":
+    pass
