dfb0684f2567b6fb44d8d4c224adf1d6b23bfb18
kitze012 <sskitzerow@gmail.com>
2024-02-13 21:00:42 -0600

    Adding py file that create tables

---
+++ init_db.py
@@ -0,0 +1,77 @@
+import sqlite3, sys, os, utils
+from tabulate import tabulate
+
+LOGGER = utils.LOGGER
+
+# ==============================================================================================================
+def create_tables():
+    '''
+    Creates the tables used by the flask server.
+
+    Parameter(s): None
+
+    Output(s): 
+        Bool: returns true if the tables are created, else returns false
+    '''
+    
+    try:
+        with sqlite3.connect('flashcards.db') as conn:      
+
+            c = conn.cursor()
+            # Force forgein key support
+            c.execute("PRAGMA foreign_keys = ON;")
+
+            # Customer Elements
+            LOGGER.info("Creating Customer Table...")
+            c.execute("""CREATE TABLE Customer(
+                    cid INTEGER PRIMARY KEY AUTOINCREMENT,
+                    cname TEXT,
+                    address TEXT,
+                    city TEXT,
+                    state TEXT
+            )""")
+        
+            # Customer contact information
+            LOGGER.info("Creating ContactInfo Table...")
+            c.execute("""CREATE TABLE ContactInfo(
+                    cid INTEGER REFERENCES Customer(cid),
+                    type TEXT, 
+                    value TEXT
+            )""")
+
+            # Owner information
+            LOGGER.info("Creating Owner Table...")
+            c.execute("""CREATE TABLE Owner(
+                    Oid INTEGER PRIMARY KEY AUTOINCREMENT,
+                    oname TEXT
+            )""")
+
+            # Restaurant information
+            LOGGER.info("Creating Restaurant Table...")
+            c.execute("""CREATE TABLE Restaurant(
+                    rid INTEGER PRIMARY KEY AUTOINCREMENT,
+                    rname TEXT,
+                    city TEXT,
+                    state TEXT,
+                    rating TEXT,
+                    ownerID INTEGER REFERENCES Customer(Oid)
+            )""")
+
+            # Reservation information
+            LOGGER.info("Creating Reservation Table...")
+            c.execute("""CREATE TABLE Reservation(
+                    cid INTEGER REFERENCES Customer(cid),
+                    rid INTEGER REFERENCES Restaurant(rid),
+                    date TEXT,
+                    num_adults INTEGER,
+                    num_child INTEGER
+            )""")
+
+            conn.commit()
+        
+        return True
+        
+    except Exception as e:
+        LOGGER.error(f"An error occured when creating tables: {e}")
+
+# ==============================================================================================================
