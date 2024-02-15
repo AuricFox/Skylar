bb9393f66db5d9322e22c336dc2a06a360a4be2a
kitze012 <sskitzerow@gmail.com>
2024-02-15 15:24:23 -0600

    Changing init_db to database.py

---
+++ database.py
@@ -0,0 +1,145 @@
+import sqlite3, sys, utils
+
+LOGGER = utils.LOGGER
+
+# ==============================================================================================================
+def create_tables():
+    '''
+    Initializes the empty tables.
+
+    Parameter(s): None
+
+    Output(s): 
+        Bool: returns true if the tables are created, else returns false
+    '''
+    
+    try:
+        with sqlite3.connect('Skylar.db') as conn:      
+
+            c = conn.cursor()
+            # Force forgein key support
+            c.execute("PRAGMA foreign_keys = ON;")
+
+            # Customer Elements
+            LOGGER.info("Creating Customer Table...")
+            c.execute("""CREATE TABLE Customer(
+                    cid INTEGER PRIMARY KEY AUTOINCREMENT,
+                    cname TEXT,
+                    address TEXT,
+                    city TEXT,
+                    state TEXT
+            )""")
+        
+            # Customer contact information
+            LOGGER.info("Creating ContactInfo Table...")
+            c.execute("""CREATE TABLE ContactInfo(
+                    cid INTEGER REFERENCES Customer(cid),
+                    type TEXT, 
+                    value TEXT
+            )""")
+
+            # Owner information
+            LOGGER.info("Creating Owner Table...")
+            c.execute("""CREATE TABLE Owner(
+                    Oid INTEGER PRIMARY KEY AUTOINCREMENT,
+                    oname TEXT
+            )""")
+
+            # Restaurant information
+            LOGGER.info("Creating Restaurant Table...")
+            c.execute("""CREATE TABLE Restaurant(
+                    rid INTEGER PRIMARY KEY AUTOINCREMENT,
+                    rname TEXT,
+                    city TEXT,
+                    state TEXT,
+                    rating TEXT,
+                    ownerID INTEGER REFERENCES Customer(Oid)
+            )""")
+
+            # Reservation information
+            LOGGER.info("Creating Reservation Table...")
+            c.execute("""CREATE TABLE Reservation(
+                    cid INTEGER REFERENCES Customer(cid),
+                    rid INTEGER REFERENCES Restaurant(rid),
+                    date TEXT,
+                    num_adults INTEGER,
+                    num_child INTEGER
+            )""")
+
+            conn.commit()
+        
+        return True
+        
+    except Exception as e:
+        LOGGER.error(f"An error occured when creating tables: {e}")
+
+# ==============================================================================================================
+def clear_tables():
+    '''
+    Clears all the data from the tables.
+
+    Parameter(s): None
+
+    Output(s): 
+        Bool: returns true if the tables are wiped, else returns false
+    '''
+
+    try:
+        with sqlite3.connect('Skylar.db') as conn:
+            c = conn.cursor()
+
+            # Customer Elements
+            LOGGER.info("Deleting Data From Customer Table...")
+            c.execute("DELETE FROM Customer")
+        
+            # Customer contact information
+            LOGGER.info("Deleting Data From ContactInfo Table...")
+            c.execute("DELETE FROM ContactInfo")
+
+            # Owner information
+            LOGGER.info("Deleting Data From Owner Table...")
+            c.execute("DELETE FROM Owner")
+
+            # Restaurant information
+            LOGGER.info("Deleting Data From Restaurant Table...")
+            c.execute("DELETE FROM Restaurant")
+
+            # Reservation information
+            LOGGER.info("Deleting Data From Reservation Table...")
+            c.execute("DELETE FROM Reservation")
+
+            conn.commit()
+
+        return True
+    
+    except sqlite3.Error as e:
+        LOGGER.error(f"An error occurred when deleting all records from the tables: {e}")
+        return False   
+
+# ==============================================================================================================
+if __name__ == "__main__":
+    '''
+    Handles command line entries to manually set the database tables
+
+    Parameter(s): 
+        Two system arguments that include a input flag: 
+        python ./init_db.py [flag]
+
+    flag(s):
+        Create tables, "-c", initializes the Flashcard and Figure tables
+        Clear tables, "-d", deletes all the rows in both tables
+        Print tables, "-p", prints all the rows in both tables
+
+    Output(s): None
+    '''
+
+    # python .\phylogeny.py input_file
+    if(len(sys.argv) != 2):
+        print(f"Only two inputs allowed, {len(sys.argv)} were entered!")
+
+    # Create Tables
+    if(sys.argv[1] == "-c"): create_tables()
+    # Delete Tables
+    elif(sys.argv[1] == "-d"): clear_tables()
+    else:
+        print("Invalid Arguments!\nCreate Tables: -c\nDelete Tables: -d\n")
