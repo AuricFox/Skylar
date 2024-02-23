fc18b009dd4369ee7d4a9ff0f45abc637f806a49
kitze012 <sskitzerow@gmail.com>
2024-02-23 13:58:00 -0600

    Adding file validation to utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,4 @@
-import logging, os, re
+import logging, os, re, mimetypes
 
 PATH = os.path.dirname(os.path.abspath(__file__))
 
@@ -16,8 +16,7 @@ LOGGER = logging.getLogger(__name__)
 class InvalidFile(Exception): pass
 class FileNotFound(Exception): pass
 class InvalidInput(Exception): pass
-# ========================================================================================================================================
-# Functions for processing text
+
 # ========================================================================================================================================
 def sanitize(text:str):
     '''
@@ -30,3 +29,45 @@ def sanitize(text:str):
         str: a sanitized string
     '''
     return re.sub(r'[\\/*?:"<>|]', '_', text)
+
+# ========================================================================================================================================
+def verify_file(file:str):
+    '''
+    Verifies input file
+
+    Parameter(s):
+        file (str): name of the provided file
+
+    Output(s):
+        True if the file is valid, else False
+    '''
+
+    try:
+        # Replace special characters with underscores
+        sanitized_name = re.sub(r'[\\/*?:"<>| ]', '_', file)
+        # Remove leading and trailing whitespace
+        sanitized_name = sanitized_name.strip()
+        # Getting the name of the file without the extension
+        sanitized_name = sanitized_name.split('.')[0]
+
+        allowed_mime_types = ['application/json']
+        allowed_extensions = ['.json']
+
+        # Get the file's MIME type and extension
+        file_mime_type, _ = mimetypes.guess_type(file)
+        file_extension = os.path.splitext(file)[1].lower()
+
+        # Check if the file's MIME type or extension is allowed
+        if file_mime_type is not None and file_mime_type not in allowed_mime_types:
+            LOGGER.error(f'{file} MIME type is not supported! MIME type: {file_mime_type}')
+            return False
+
+        if file_extension not in allowed_extensions:
+            LOGGER.error(f'{file} extension is not supported! Extension: {file_extension}')
+            return False
+        
+        return True
+
+    except Exception as e:
+        LOGGER.error(f"An error occured when validating {file}: {e}")
+        return False
