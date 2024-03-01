d4fbbbc6902adccedec40b41fcd9ee3e13b70790
kitze012 <sskitzerow@gmail.com>
2024-02-29 19:10:31 -0600

    Changing entry method in wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
     Set host='0.0.0.0' to make the server externally visible
     Set port=5000 to use port 5000 (default for Flask)
     '''
-    app.run()
+    app.run(host='0.0.0.0')
