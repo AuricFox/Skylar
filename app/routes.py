577cb8bd34478644ae01a3f2d61eff69a5790e7f
kitze012 <sskitzerow@gmail.com>
2024-03-04 00:05:18 -0600

    Adding favicon

--- app/routes.py
+++ app/routes.py
@@ -38,3 +38,9 @@ def home():
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html'), 404
+
+# ====================================================================
+# Custom favicon
+@app.route('/favicon.ico')
+def favicon():
+    return app.send_static_file('images/favicon.ico')
