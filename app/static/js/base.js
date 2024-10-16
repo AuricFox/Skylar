c42e7d7993907e9fb00975a42b7f26410f0575b6
kitze012 <sskitzerow@gmail.com>
2024-10-15 23:12:26 -0500

    Adding prompt window and event listener to base js

--- app/static/js/base.js
+++ app/static/js/base.js
@@ -1,1 +1,12 @@
-// Base js file
+// Confirm database reset
+$('.reset-database').click(function () {
+    confirmReset();
+});
+
+// Display window prompt
+function confirmReset() {
+    var result = confirm("Are you sure you want to reset the database?");
+    if (result) {
+        window.location.href = "reset_database/";
+    }
+};
